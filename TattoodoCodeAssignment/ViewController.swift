de890d797bfcfd6d8860c9b2707c90ad58e855fa
iosypenko-axon <46967944+iosypenko-axon@users.noreply.github.com>
2020-03-05 15:00:40 +0200

    Add files via upload

---
+++ TattoodoCodeAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TattoodoCodeAssignment
+//
+//  Created by Ivan Osypenko on 05/03/2020.
+//  Copyright © 2020 iosypenk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
